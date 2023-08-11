bce2bae3100b8dcb6bf80d678b8582afc83bb8d3
KakaVin <Ragylin1@mail.ru>
2023-08-11 15:47:33 +0500

    Пешка и тесты к ней

--- lib/Pieces/pawn.dart
+++ lib/Pieces/pawn.dart
@@ -1,7 +1,10 @@
+import 'dart:collection';
+
 import 'package:chess_offline/Pieces/util/color_chess.dart';
 import 'package:chess_offline/Pieces/util/coordinates.dart';
 import 'package:chess_offline/Pieces/piece.dart';
 
+import '../board.dart';
 import 'util/coordinates_shift.dart';
 
 class Pawn extends Piece {
@@ -9,7 +12,43 @@ class Pawn extends Piece {
 
   @override
   Set<CoordinatesShift> getPieceMove() {
-    // TODO: implement getPieceMove
-    throw UnimplementedError();
+    Set<CoordinatesShift> result = HashSet();
+
+    if (color == ColorChess.white) {
+      {
+        result.add(CoordinatesShift(0, 1));
+
+        if (coordinates.rank == 2) {
+          result.add(CoordinatesShift(0, 2));
+        }
+
+        result.add(CoordinatesShift(1, 1));
+        result.add(CoordinatesShift(-1, 1));
+      }
+    } else {
+      result.add(CoordinatesShift(0, -1));
+
+      if (coordinates.rank == 7) {
+        result.add(CoordinatesShift(0, -2));
+      }
+
+      result.add(CoordinatesShift(1, -1));
+      result.add(CoordinatesShift(-1, -1));
+    }
+
+    return result;
+  }
+
+  @override
+  bool isSquareAvailableForMove(Coordinates coordinates, Board board) {
+    if (coordinates.file == this.coordinates.file) {
+      return board.isSquareEmpty(coordinates);
+    } else {
+      if (board.isSquareEmpty(coordinates)){
+        return false;
+      } else {
+        return board.getPiece(coordinates).color != color;
+      }
+    }
   }
 }
